c96d7b309bdfbb5959f65fe0269c086d403c89ff
rlackftm1818 <76882003+rlackftm1818@users.noreply.github.com>
2021-01-04 22:56:50 +0900

    Create index.php

---
+++ docs/land/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>사이트안내</title>
+    <style type="text/css">
+      *{margin:0;padding:0;outline:0;font-size:1em;font-weight:normal;font-style:normal;border:0;text-decoration:none;list-style-type:none}
+      body{background: #000;}
+      .container{
+        width: 100%;
+        max-width: 980px;
+        margin: 0 auto;
+      }
+      .bg{width: 100%;}
+      .btn{position:absolute;top:0;left:512px;}
+      .b1{margin: 300px 0px 10px calc((100% - 980px) * 0.5);}
+      .b2{margin: 570px 0px 10px calc((100% - 980px) * 0.5);}
+      .b3{margin: 840px 0px 10px calc((100% - 980px) * 0.5);}
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <img class="bg" src="/web/img/land_bg.webp">
+      <a class="btn b1" href="http://양장피.kr">
+        <img src="/web/img/land_btn.gif">
+      </a>
+      <a class="btn b2" href="http://엑스투.kr">
+        <img src="/web/img/land_btn.gif">
+      </a>
+      <a class="btn b3" href="http://전봇대.kr">
+        <img src="/web/img/land_btn.gif">
+      </a>
+    </div>
+  </body>
+</html>
